21d2c4627e8e0b70b1d87ed084afe2529b8bcee2
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2024-11-23 09:08:05 +0530

    Add files via upload

---
+++ 01/program3.c
@@ -0,0 +1,20 @@
+//3. Program to print 5 to 1 numbers on screen.
+#include<stdio.h>
+
+void Display()
+{
+    //int i=0;
+    int i=5;
+    while(i>=1)
+    {
+        printf("%d\n",i);
+        i--;
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
